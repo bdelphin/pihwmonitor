bf957e57ff4e27bf10f166d477d8167287519064
Baptiste DELPHIN <baptiste.delphin@gmail.com>
2021-01-11 00:22:54 +0100

    added RPi shutdown on hwmonitor service stop. Not sure it'll work.

--- hwmonitor_stop.sh
+++ hwmonitor_stop.sh
@@ -11,5 +11,9 @@
 /usr/bin/rm /tmp/radeontop.log
 /usr/bin/echo "Radeontop dump stopped."
 
+# shutdown the RPi
+/usr/bin/curl http://192.168.42.5:5000/shutdown
+/usr/bin/sleep 5
+
 /usr/bin/echo "PiHWMonitor service stopped."
 
